ebfd0c196cd7598715d05208656de1b6f84d2b4a
Michael <miichaaeell.andrew@gmail.com>
2025-08-19 11:34:40 -0300

    Created folders to separate the templates

--- equipment/views.py
+++ equipment/views.py
@@ -6,41 +6,41 @@ from .models import Equipment, Brand, ModelEquipment, Category, StatusEquipment
 from .forms import EquipmentForm, BrandForm, ModelEquipmentForm, CategoryForm, StatusEquipmentForm
 
 
-#Views Equipment
+# Views Equipment
 class CreateEquipmentView(FormValidMixin, CreateView):
     model = Equipment
     form_class = EquipmentForm
-    template_name = 'create_equipment.html'
+    template_name = 'equipment/create_equipment.html'
     success_url = reverse_lazy('stock')
 
 
-#Views Brand
+# Views Brand
 class CreateBrandView(FormValidMixin, CreateView):
     model = Brand
     form_class = BrandForm
-    template_name = 'create_brand.html'
+    template_name = 'brand/create_brand.html'
     success_url = reverse_lazy('stock')
 
 
-#Views Model Equipment
+# Views Model Equipment
 class CreateModelEquipmentView(FormValidMixin, CreateView):
     model = ModelEquipment
     form_class = ModelEquipmentForm
-    template_name = 'create_model_equipment.html'
+    template_name = 'model/create_model_equipment.html'
     success_url = reverse_lazy('stock')
 
 
-#Views Category 
+# Views Category
 class CreateCategoryView(FormValidMixin, CreateView):
     model = Category
     form_class = CategoryForm
-    template_name = 'create_category.html'
+    template_name = 'category/create_category.html'
     success_url = reverse_lazy('stock')
 
 
-#Views Status
+# Views Status
 class CreateStatusView(FormValidMixin, CreateView):
     model = StatusEquipment
     form_class = StatusEquipmentForm
-    template_name = 'create_status_equipment.html'
+    template_name = 'status/create_status_equipment.html'
     success_url = reverse_lazy('stock')
